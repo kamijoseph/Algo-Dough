cbd0a92ebc7d719738b8fe55b052d6eae495f39b
Kami <josephkiarie561@gmail.com>
2026-08-27 14:58:32 +0300

    downlaod daata function

--- Scripts/data.py
+++ Scripts/data.py
@@ -3,3 +3,16 @@
 
 import yfinance as yf
 import pandas as pd
+
+# downloading the data
+def download_data(ticker, start, end, interval):
+    data = yf.download(
+        ticker,
+        start = start,
+        end = end,
+        interval = interval,
+        auto_adjust = False,
+        actions = True
+    )
+
+    return data
